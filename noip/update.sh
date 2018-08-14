2bf21b93f1f965da9f65120d04471d75e61b20f4
Yz <yanzhen610@gmail.com>
2018-08-15 04:09:11 +0800

    added noip

---
+++ noip/update.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+sigterm()
+{
+  echo '"docker stop"' detected
+  echo stopping now
+  exit 0
+}
+
+trap 'sigterm' TERM
+
+if [ -z "$NOIP_USERNAME" ]
+then
+  echo username is required
+  echo use '"-e NOIP_USERNAME=your-username"' to set username
+  exit 1
+fi
+
+if [ -z "$NOIP_PASSWORD" ]
+then
+  echo password is required
+  echo use '"-e NOIP_PASSWORD=your-password"' to set password
+  exit 1
+fi
+
+if [ -z "$NOIP_HOSTNAME" ]
+then
+  echo host name is required
+  echo user '"-e NOIP_HOSTNAME=your-hostname"' to set hostname
+  exit 1
+fi
+
+if [ -z "$PERIOD" ]
+then
+  PERIOD=30
+elif ! [[ $PERIOD =~ '^[0-9]+$' ]]
+then
+  echo '$PREIOD' should be a positive integer
+  exit 2
+fi
+
+while :
+do
+  /usr/bin/curl -s https://$NOIP_USERNAME:$NOIP_PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$NOIP_HOSTNAME
+  sleep $PERIOD &
+  wait $!
+done
+
